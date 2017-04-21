fa0f41a4d442b2bbe7fbfe4a5b6413b61f29b48e
huageng <stehliu25@gmail.com>
2017-04-21 17:12:00 +0800

    ajust tile postion so now they can perfectly fit in the container

---
+++ js/game_controller.js
@@ -0,0 +1,44 @@
+function gameController(size, actuator){
+	this.size = size;
+
+}
+
+gameController.prototype.setup = function(){
+
+};
+
+gameController.prototype.buildGrid = function(){
+
+};
+
+gameController.prototype.addStartTiles = function(){
+
+};
+
+gameController.prototype.addTile = function(){
+
+
+};
+
+gameController.prototype.randomCell= function(){
+
+};
+
+gameController.prototype.cellOccupied = function(){
+
+};
+
+gameController.prototype.insertTile = function(){
+
+};
+
+
+gameController.prototype.update = function(){
+
+};
+
+
+gameController.prototype.move = function(){
+ 	//0: up , 1: right; 2:down; 3: left
+	this.update();
+};
